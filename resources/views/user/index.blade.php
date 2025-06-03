886b24decea9fdca8bbbd92719c67327151f1191
Trixieee-dev <126485215+Trixieee-dev@users.noreply.github.com>
2025-06-03 16:07:30 +0700

    update Image

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,13 +1,16 @@
 @extends('layout.user.nav')
 @section('content')
 
-<div class="hero-wrap js-fullheight" style="background-image: url('images/bg_5.jpg');">
+<div class="hero-wrap js-fullheight" style="background-image: url('images/bg_main.jpg');">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
             <div class="col-md-7 ftco-animate">
                 <h1 class="mb-4">
-                    Bersama Kembangkan Pertanian Tapanuli Utara
+                    Tubalian <br>
+                    Tapanuli Utara <br>
+                    Bangga Lihat <br>
+                    Pertanian
                 </h1>
                 <!-- <h2 class="mb-4">asdasd </h2> -->
             </div>
